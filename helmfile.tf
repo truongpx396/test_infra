2f55868e07d821952f6817f267a2a3da8c5328d6
xphung@mantu.com <xphung@mantu.com>
2021-08-22 21:40:07 +0700

    update helmfile

--- helmfile.tf
+++ helmfile.tf
@@ -2,5 +2,5 @@ provider "helmfile" {}
 
 resource "helmfile_release_set" "mystack" {
     kubeconfig = module.eks.kubeconfig
-    content = file("./helmfile.yaml")
+    content = file("${path.module}/helmfile.yaml")
 }
