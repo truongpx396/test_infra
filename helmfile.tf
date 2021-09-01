635cf71958875db204647bc0df7e346911c350fd
Phung Xuan Truong <phungxuantruong@Phungs-MacBook-Pro.local>
2021-09-01 14:12:43 +0700

    Update

--- helmfile.tf
+++ helmfile.tf
@@ -1,17 +1,17 @@
-provider "helmfile" {}
+# provider "helmfile" {}
 
-resource "helmfile_release_set" "mystack" {
-    version = "0.128.0"
-    helm_version = "3.2.1"
-    helm_diff_version = "v3.1.3"
-    kubeconfig = module.eks.kubeconfig
-    content = file("./helmfile.yaml")
-}
+# resource "helmfile_release_set" "mystack" {
+#     version = "0.128.0"
+#     helm_version = "3.2.1"
+#     helm_diff_version = "v3.1.3"
+#     kubeconfig = module.eks.kubeconfig
+#     content = file("./helmfile.yaml")
+# }
 
-output "mystack_diff" {
-  value = helmfile_release_set.mystack.diff_output
-}
+# output "mystack_diff" {
+#   value = helmfile_release_set.mystack.diff_output
+# }
 
-output "mystack_apply" {
-  value = helmfile_release_set.mystack.apply_output
-}
+# output "mystack_apply" {
+#   value = helmfile_release_set.mystack.apply_output
+# }
